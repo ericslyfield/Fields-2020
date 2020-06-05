d746f65fe6dceab5f01172a5267ead770395f983
Eric Slyfield <eric.slyfield@gmail.com>
2020-06-05 09:30:50 -0400

    Updates Scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-	alert('results may vary.');
-});
-
-
-$(document).ready(function() {
-
-var wavesurfer = WaveSurfer.create({
-    container: '#analyzer',
-    cursorWidth: 0,
-    waveColor: 'grey',
-    progressColor: 'darkgreen',
-    //barGap: 2,
-	barWidth: 3,
-	barRadius: 5,
-	hideScrollbar: true,
-});
-
-$(".play-btn").click(function(e){
-	wavesurfer.playPause();
-});
-
-});
-=======
-
 	alert('-e.');
-})
->>>>>>> 96e19c02d8d09051bcde05434c0419ba7d67a0b6
+});
+
+
